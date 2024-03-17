08c6e70fd5bdeb6eb00bdbb9aef87305836a7d8a
puyan <puyanz15@gmail.com>
2024-03-17 11:17:22 +0330

    adding and connecting templates to views and urls.

--- todo/urls.py
+++ todo/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.show, name='index'),
+    path('index', views.add, name='index'),
+    path('index', views.edit, name='index'),
+    path('index', views.delete, name='index'),
+    path('index', views.list, name='index'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
-from .forms import AddForm
+from .models import Task
 
 # Create your views here.
-def show(request):
+def add(request):
+    context = {}
+    if request.method == 'POST':
+        task = Task(title = request.POST['title'], done = request.POST['done'])
+        task.save()
+        context = {'task': task}
+    return render(request, 'index.html', context=context)
+
+
+def edit(request, id):
+    context = {}
+    if request.method == 'POST':
+        task = Task.objects.filter(id=id)
+        if task is not None:
+            task.title = request.POST['title']
+            task.done = request.POST['done']
+            task.save()
+        context = {'task': task}
+    return render(request, 'index.html', context=context)
+
+def delete(request, id):
+    if request.method == 'DELETE':
+        Task.objects.filter(id=id).delete() 
+    return render(request, 'index.html')
+
+def list(request):
+    context = {}
+    if request.method == 'GET':
+        tasks = Task.objects.all()
+        context = {'tasks': tasks}
+    return render(request, 'index.html', context)
